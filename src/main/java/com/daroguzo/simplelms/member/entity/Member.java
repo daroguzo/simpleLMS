6d25e0a0dc414a794e2dc1e66295fb737d59e8ae
daroguzo <kjw2122@naver.com>
2021-12-15 23:41:29 +0900

    add: Member entity

---
+++ src/main/java/com/daroguzo/simplelms/member/entity/Member.java
@@ -0,0 +1,23 @@
+package com.daroguzo.simplelms.member.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private long id;
+
+    private String userId;
+    private String username;
+    private String phone;
+    private String password;
+    private LocalDateTime regDt;
+}

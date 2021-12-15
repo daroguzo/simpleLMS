cb117d28a2ab88ae3bb62173831f6aab5d9f59d8
daroguzo <kjw2122@naver.com>
2021-12-15 20:33:48 +0900

    Initial commit

---
+++ src/main/java/com/daroguzo/simplelms/SimpleLmsApplication.java
@@ -0,0 +1,13 @@
+package com.daroguzo.simplelms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daroguzo/simplelms/SimpleLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daroguzo.simplelms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

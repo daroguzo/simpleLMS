3cde181f4c065041081eb0e835d4d31fae0940ae
daroguzo <kjw2122@naver.com>
2021-12-21 15:41:17 +0900

    modify: email 승인에 필요한 mapping

--- src/main/java/com/daroguzo/simplelms/member/controller/MemberController.java
+++ src/main/java/com/daroguzo/simplelms/member/controller/MemberController.java
@@ -7,26 +7,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/member/register")
+    @GetMapping("/register")
     public String register() {
         return "member/register";
     }
 
-    @PostMapping("/member/register")
+    @PostMapping("/register")
     public String registerSubmit(Model model, HttpServletRequest request, MemberDto memberDto) {
 
         boolean result = memberService.register(memberDto);
         model.addAttribute("result", result);
 
         return "member/register_complete";
+    }
+
+    @GetMapping("/email-auth")
+    public String emailAuthorize(@RequestParam("uuid") String uuid, Model model) {
+
+        boolean result = memberService.emailAuth(uuid);
+        System.out.println(result);
+        model.addAttribute("result", result);
+
+        return "member/email-auth";
     }
 }

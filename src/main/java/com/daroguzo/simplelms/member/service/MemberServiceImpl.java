147a52ab0041b2b12dbdc0d398170880d1c9d2d6
daroguzo <kjw2122@naver.com>
2021-12-21 14:52:12 +0900

    add: 가입 승인 메일 보내기 기능 추가

--- src/main/java/com/daroguzo/simplelms/member/service/MemberServiceImpl.java
+++ src/main/java/com/daroguzo/simplelms/member/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.daroguzo.simplelms.member.service;
 
+import com.daroguzo.simplelms.component.MailComponents;
 import com.daroguzo.simplelms.member.entity.Member;
 import com.daroguzo.simplelms.member.model.MemberDto;
 import com.daroguzo.simplelms.member.repository.MemberRepository;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ import java.util.Optional;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
+    private final MailComponents mailComponents;
 
     /**
      * 회원 가입
@@ -30,14 +33,29 @@ public class MemberServiceImpl implements MemberService{
             return false;
         }
 
+        String uuid = UUID.randomUUID().toString();
+
         Member member = new Member();
         member.setEmail(memberDto.getEmail());
         member.setUsername(memberDto.getUsername());
         member.setPassword(memberDto.getPassword());
         member.setPhone(memberDto.getPhone());
         member.setRegDt(LocalDateTime.now());
+        member.setEmailAuthorized(false);
+        member.setEmailAuthKey(uuid);
 
         memberRepository.save(member);
+
+        sendAuthEmail(memberDto.getEmail(), uuid);
+
         return true;
+    }
+
+    private void sendAuthEmail(String email, String uuid) {
+        String subject = "LMS 시스템에 오신 것을 환영합니다.";
+        String text = "<h2>LMS 시스템 가입 안내<h2>" +
+                "<p>아래 링크를 클릭하고 가입 절차를 완료하세요.</p>" +
+                "<div><a href='http://localhost:8080/member/email-auth?id=" + uuid + "'>가입 완료</a></div>";
+        mailComponents.sendMail(email, subject, text);
     }
 }

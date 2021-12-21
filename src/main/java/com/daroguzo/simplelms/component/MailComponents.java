22b5195a7197338fc5f2b90041d726bbb885b3f6
daroguzo <kjw2122@naver.com>
2021-12-21 13:25:44 +0900

    modify: sendMail() 생성 및 테스트

--- src/main/java/com/daroguzo/simplelms/component/MailComponents.java
+++ src/main/java/com/daroguzo/simplelms/component/MailComponents.java
@@ -3,7 +3,11 @@ package com.daroguzo.simplelms.component;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
 
 @RequiredArgsConstructor
 @Component
@@ -19,5 +23,28 @@ public class MailComponents {
         message.setText("안녕하세요 내용입니다.");
 
         javaMailSender.send(message);
+    }
+
+    public boolean sendMail(String email, String subject, String text) {
+
+        boolean result = false;
+
+        MimeMessagePreparator message = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                mimeMessageHelper.setTo(email);
+                mimeMessageHelper.setSubject(subject);
+                mimeMessageHelper.setText(text, true);
+            }
+        };
+        try {
+            javaMailSender.send(message);
+            result = true;
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
     }
 }
